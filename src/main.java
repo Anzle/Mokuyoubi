f8dc8bf2c6d02e434ea08bcd81eb01916c35e4b7
rpurwah <rahulpurwah@gmail.com>
2014-10-06 23:34:40 -0400

    updated main which reads in byte array
    let me know if this works for you guys

--- src/main.java
+++ src/main.java
@@ -1,3 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import GivenTools.BencodingException;
+
 
 public class main{
 
@@ -22,6 +31,66 @@ public class main{
 //the following is a check to make sure the command line arguments were stored correctly
 	System.out.println("tfile: "+tfile);  
 	System.out.println("sfile: "+sfile);
+	
+	
+	
+	
+	File file = new File(tfile);
+	long fsize = -1;
+	byte[] tbytes = null;
+	InputStream file_stream;
+
+	try
+	{
+		file_stream = new FileInputStream(file);
+		fsize = file.length();
+		
+		// Initialize the byte array for the file's data
+		tbytes = new byte[(int) fsize];
+
+		int offset = 0;
+		int aread = 0;
+
+		// Read from the file
+		while (offset < tbytes.length
+				&& (aread = file_stream.read(tbytes, offset,
+						tbytes.length - offset)) >= 0)
+		{
+			offset += aread;
+		}
+
+		file_stream.close();
+
+	}
+	catch (FileNotFoundException e)
+	{
+		System.err
+				.println("Error: [TorrentFileHandler.java] The file \""
+						+ tfile
+						+ "\" does not exist. Please make sure you have the correct path to the file.");
+		return;
+	}
+	catch (IOException e)
+	{
+		System.err
+				.println("Error: [TorrentFileHandler.java] There was a general, unrecoverable I/O error while reading from \""
+						+ tfile + "\".");
+		System.err.println(e.getMessage());
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
